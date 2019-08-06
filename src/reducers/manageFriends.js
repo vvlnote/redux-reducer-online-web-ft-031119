b283607bc16a50222fd7d1cd2836840b9d383d5e
Joy Lin <vvlnote@hotmail.com>
2019-08-06 01:08:27 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -4,7 +4,7 @@ export function manageFriends(state, action){
   switch (action.type) {
 
     case 'ADD_FRIEND':
-      return{friends: (...state.friends, newFriend, {name: action.friend} )};
+      return(...state.friends, newFriend, {name: action.friend} );
     default:
       return state;
   }
