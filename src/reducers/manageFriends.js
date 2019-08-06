898d39a1b2b7536d2a64d322db11094e3448ccbe
Joy Lin <vvlnote@hotmail.com>
2019-08-06 01:18:06 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,26 @@
 export function manageFriends(state, action){
+
+  let newFriend = {};
+  switch (action.type) {
+
+    case 'ADD_FRIEND':
+      return({...state,
+        friends: [
+          ...state.friends,
+            action.friend
+        ]
+        });
+
+    case 'REMOVE_FRIEND':
+      const index = state.friends.findIndex(friend => friend.id === action.id);
+      return(
+        {...state,
+          friends: [
+            ...state.friends.slice(0, index),
+            ...state.friends.slice(index+1)
+          ]}
+      )
+    default:
+      return state;
+  }
 }
