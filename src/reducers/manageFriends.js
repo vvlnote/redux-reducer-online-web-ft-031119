98282bc43f34b5b62177c599257eb67627501ac0
Joy Lin <vvlnote@hotmail.com>
2019-08-06 01:16:40 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -10,6 +10,16 @@ export function manageFriends(state, action){
             action.friend
         ]
         });
+
+    case 'REMOVE_FRIEND':
+      const index = state.friends.findIndex(friend => friend.id === action.id);
+      return(
+        {...state,
+          friends: [
+            ...state.friends.slice(0, index),
+            ...state.friends.slice(index+1)
+          ]}
+      )
     default:
       return state;
   }
